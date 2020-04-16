9ce440ec89f64092a40f3bd13a3996a54441b231
pashkett <pashkett@gmail.com>
2020-04-16 19:25:17 +0300

    fix formatting

--- FileStorageBackend/FileStorage.API/Extensions/ExceptionMiddlewareExtensions.cs
+++ FileStorageBackend/FileStorage.API/Extensions/ExceptionMiddlewareExtensions.cs
@@ -30,7 +30,7 @@ namespace FileStorage.API.Extensions
             });
         }
 
-        public static void AddApplicationError(this HttpResponse response, string message)
+        private static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
--- FileStorageBackend/FileStorage.API/Startup.cs
+++ FileStorageBackend/FileStorage.API/Startup.cs
@@ -41,7 +41,9 @@ namespace FileStorage.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
+        public void Configure(IApplicationBuilder app,
+                              IWebHostEnvironment env,
+                              ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
