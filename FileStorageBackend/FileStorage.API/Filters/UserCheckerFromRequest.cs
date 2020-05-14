3e8ef36d09321011fdc4a4273ce05f8b6cf5e494
pashkett <pashkett@gmail.com>
2020-05-14 13:35:03 +0300

    refactor action filter

--- FileStorageBackend/FileStorage.API/Filters/UserCheckerFromRequest.cs
+++ FileStorageBackend/FileStorage.API/Filters/UserCheckerFromRequest.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using FileStorage.Domain.Services.AuthenticationServices;
 
 namespace FileStorage.API.Filters
@@ -11,16 +12,13 @@ namespace FileStorage.API.Filters
     {
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            var configuration =
-                context.HttpContext.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
+            var configuration = context.HttpContext.RequestServices.GetService<IConfiguration>();
 
             string userParamName = configuration.GetValue<string>("UserKeyParameter");
 
-            var authService =
-                context.HttpContext.RequestServices.GetService(typeof(IAuthService)) as IAuthService;
+            var authService = context.HttpContext.RequestServices.GetService<IAuthService>();
 
-            var userRequested =
-                await authService.GetRequestedUser(context.HttpContext.User);
+            var userRequested = await authService.GetRequestedUser(context.HttpContext.User);
 
             if (userRequested == null)
             {
